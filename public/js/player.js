4670681aabf7ecbce91dcef4e07baf2fa7615242
Hans Quiogue <hansquiogue@gmail.com>
2020-11-18 20:57:30 -0500

    Added play, rewind, fast forward functionality

--- public/js/player.js
+++ public/js/player.js
@@ -9,7 +9,9 @@ window.onSpotifyWebPlaybackSDKReady = async function() {
     const uriBody = await uriResponse.json();
 
     // First album is initial uri
-    let spotify_uri = uriBody[0][0]['album-tracks-uri'].items[0].uri;
+    let spotifyURI = uriBody[0][0]['album-uri'];
+
+    console.log(spotifyURI);
 
     // Milk Crate Player
     const player = new Spotify.Player({
@@ -24,22 +26,41 @@ window.onSpotifyWebPlaybackSDKReady = async function() {
     player.addListener('playback_error', ({ message }) => { console.error(message); });
 
     // Playback status updates
-    player.addListener('player_state_changed', state => { console.log(state); });
+    player.addListener('player_state_changed', state => { 
+        
+        // Whenever back button is clicked
+        document.getElementById('stepBackward').addEventListener('click', () => player.previousTrack());
+
+        // Whenever forward button is clicked
+        document.getElementById('stepForward').addEventListener('click', () => player.nextTrack());
+        
+        // Whenever play or pause button is clicked
+        $('#playPauseButton').on('click', () => pauseOrPlay(player));
+
+        console.log('Current state');
+        console.log(state); 
+    });
 
     // Ready
     player.addListener('ready', async ({ device_id }) => {
-        // Plays current 
+
+        
+        // Plays first song in first album selected
         await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, 
         {
             method: 'PUT',
-            body: JSON.stringify({ uris: [spotify_uri] }),
+            body: JSON.stringify({"context_uri": spotifyURI}),
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
             },
         });
 
-        console.log('Ready with Device ID', device_id);
+        // Whenever play or pause button is clicked
+        $('#playPauseButton').on('click', () => pauseOrPlay(player));
+        
+        // Logs that device is ready
+        // console.log('Ready with Device ID', device_id);
     });
 
     // Not Ready
@@ -50,3 +71,26 @@ window.onSpotifyWebPlaybackSDKReady = async function() {
     // Connect to the player!
     player.connect();
 };
+
+/**
+ * Pauses or plays the current song.
+ * @param {Object<Spotify Player>} player A spotify player object
+ */
+function pauseOrPlay(player) {
+    player.togglePlay().then(() => {
+        const button = document.getElementById('playPauseButton');
+        const className = Array.from(button.classList);
+
+        // Button is currently circle
+        if (className.includes('fa-play-circle')) {
+            // Change button to pause
+            button.classList.remove('fa-play-circle');
+            button.classList.add('fa-pause-circle');
+        // Button is currently paused
+        } else {
+            // Change to paly
+            button.classList.remove('fa-pause-circle');
+            button.classList.add('fa-play-circle');
+        }
+    });
+}
